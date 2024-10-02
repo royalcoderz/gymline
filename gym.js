9bb055dbb0cae6031872a61e982192d6741aa069
royalcoderz <royalcoders255@gmail.com>
2024-10-02 15:01:31 +0530

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll',function(){
+    const navbar =
+    document.getElementById('navbar');
+    if(window.scrollY>50){
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+});
+alert("thanks to visit my website")
